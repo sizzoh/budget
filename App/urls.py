90ed889a4f8bd6aaa24727a372523937f33b9d7c
sizzoh <greysonpeter37@gmail.com>
2024-03-30 20:16:15 +0300

    updating requirements

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('home', views.home, name='home'),
